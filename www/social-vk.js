0d2a587077d63000516f6fc4bb195334facdd33b
Vasiliy Makarov <drmoriarty.0@gmail.com>
2015-05-01 23:06:53 +0300

    fix plugin js

--- www/social-vk.js
+++ www/social-vk.js
@@ -1,17 +1,16 @@
-cordova.define("ru.trilan.cordova-social-vk.SocialVk", function(require, exports, module) { function SocialVk() {
-  // Does nothing
-}
-SocialVk.prototype.init = function(appId, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "SocialVk", "initSocialVk", [appId]);
+function SocialVk() {
+    // Does nothing
 };
-               
-               SocialVk.prototype.login = function(successCallback, errorCallback) {
-               cordova.exec(successCallback, errorCallback, "SocialVk", "login");
-               }
+SocialVk.prototype.init = function(appId, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "SocialVk", "initSocialVk", [appId]);
+};
+
+SocialVk.prototype.login = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "SocialVk", "login");
+};
 
 SocialVk.prototype.share = function(sourceURL, description, imageURL, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "SocialVk", "share", [sourceURL, description, imageURL]);
+    cordova.exec(successCallback, errorCallback, "SocialVk", "share", [sourceURL, description, imageURL]);
 };
 module.exports = new SocialVk();
 
-});
