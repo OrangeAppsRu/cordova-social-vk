c85d83002b5ec4564f2be8f979a26aa6a9c7a220
Василий Макаров < >
2015-11-20 15:37:20 +0400

    add stub sources for wp8

---
+++ src/wp/SocialVk.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using Windows.ApplicationModel.Store;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+using Newtonsoft.Json.Linq;
+using Odnoklassniki;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+
+    public class SocialOk : BaseCommand
+    {
+        public void initSocialVk(string par)
+        {
+
+        }
+
+        public void login(string par)
+        {
+
+        }
+
+        public void share(string par)
+        {
+
+        }
+
+        public void logout(string par)
+        {
+
+        }
+
+        public void users_get(string par)
+        {
+
+        }
+
+        public void users_search(string par)
+        {
+
+        }
+
+        public void users_isAppUser(string par)
+        {
+
+        }
+
+        public void users_getSubscription(string par)
+        {
+
+        }
+
+        public void users_getFollowers(string par)
+        {
+
+        }
+
+        public void wall_post(string par)
+        {
+
+        }
+
+        public void photos_getUploadServer(string par)
+        {
+
+        }
+
+        public void photos_getWallUploadServer(string par)
+        {
+
+        }
+
+        public void photos_saveWallPhoto(string par)
+        {
+
+        }
+
+        public void photos_save(string par)
+        {
+
+        }
+
+        public void friends_get(string par)
+        {
+
+        }
+
+        public void friends_getOnline(string par)
+        {
+
+        }
+
+        public void friends_getMutual(string par)
+        {
+
+        }
+
+        public void friends_getRecent(string par)
+        {
+
+        }
+
+        public void friends_getRequests(string par)
+        {
+
+        }
+
+        public void callApiMethod(string par)
+        {
+
+        }
+
+    }
+}
