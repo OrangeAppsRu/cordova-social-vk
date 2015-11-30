464ea048ea0ea4aa4349966f658915fd25785f85
Vasiliy Makarov <drmoriarty.0@gmail.com>
2015-11-30 10:03:47 +0300

    stub for windows universal

---
+++ src/windows/SocialVk.js
@@ -0,0 +1,63 @@
+module.exports = {
+    initSocialVk: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    login: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    share: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    logout: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    // API methods
+    users_get: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    users_search: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    users_isAppUser: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    users_getSubscriptions: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    users_getFollowers: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    wall_post: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    photos_getUploadServer: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    photos_getWallUploadServer: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    photos_saveWallPhoto: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    photos_save: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    friends_get: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    friends_getOnline: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    friends_getMutual: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    friends_getRecent: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    friends_getRequests: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+    callApiMethod: function(success, error, args) {
+        error('Method not implemented yet');
+    },
+}
